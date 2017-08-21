ee14ebbb0a8a9b9997ca34608eb92da2c35c9d91
Erik Sorensen <sorensen.erik48@gmail.com>
2017-08-21 15:14:49 -0400

    Update trendline.py

--- trendline.py
+++ trendline.py
@@ -1,4 +1,4 @@
-# Credits to @Micah on Stack Exchange
+# Credits to @Micah on StackOverflow
 # got this function from https://stackoverflow.com/questions/22239691/code-for-line-of-best-fit-of-a-scatter-plot-in-python
 # Added some of my own code to increase redability and made it work with NaN's in the data
 
